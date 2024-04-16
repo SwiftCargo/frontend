f12d77ac291cee163e08261a358c276db3140801
pikushe <piikushe@gmail.com>
2024-04-16 20:25:32 +0300

    Home page finished

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,10 @@
 
 import InformationCard from "@/components/views/Home/InformationCard";
 import Chart from "@/components/views/Home/Chart";
-import { RiTruckLine, RiPinDistanceLine, RiMoneyEuroCircleLine, RiCheckLine } from '@remixicon/react';
+
+import { RiTruckLine, RiPinDistanceLine, RiMoneyEuroCircleLine } from '@remixicon/react';
+import Image from "next/image";
+import HomeCarousel from "@/components/views/Home/Carousel";
 export default function Home() {
   const DistanceData = {
     labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
@@ -38,56 +41,73 @@ export default function Home() {
     ]
   };
   return (
-    <div className="flex flex-col gap-6 justify-between md:py-6 md:pr-6 w-full">
-      <div>
-        <h1 className="text-3xl font-bold">Hello<span className="font-thin">, pikushe 👋🏻</span></h1>
+    <div className="flex flex-col gap-4 justify-between md:py-6 md:pr-6 w-full">
+      <div className="flex flex-row gap-2 ">
+        <div className="flex flex-col gap-4 w-full">
+          <div>
+            <h1 className="text-3xl font-bold">Hello<span className="font-thin">, pikushe 👋🏻</span></h1>
+          </div>
+          <div className="flex flex-row justify-between w-full">
+            <InformationCard>
+              <div className="p-4 bg-[#3a3b64] rounded-full">
+                <RiTruckLine size={35} color="#7367f0" />
+              </div>
+              <div className="flex flex-col text-center gap-1 w-full">
+                <h1 className="text-xl md:text-4xl">9</h1>
+                <h4 className="text-xs md:text-sm">Total Jobs</h4>
+              </div>
+            </InformationCard>
+            <InformationCard>
+              <div className="p-4 bg-[#64633a] rounded-full">
+                <RiPinDistanceLine size={35} color="#ebf55a" />
+              </div>
+              <div className="flex flex-col text-center gap-1 w-full">
+                <h1 className="text-xl md:text-4xl">3,554<span className="text-xl">Km</span></h1>
+                <h4 className="text-xs md:text-sm">Total Distance</h4>
+              </div>
+            </InformationCard>
+            <InformationCard>
+              <div className="p-4 bg-[#3b643a] rounded-full">
+                <RiMoneyEuroCircleLine size={35} color="#2ac926" />
+              </div>
+              <div className="flex flex-col text-center gap-1 w-full">
+                <h1 className="text-xl md:text-4xl">250,310<span className="text-xl">€</span></h1>
+                <h4 className="text-xs md:text-sm">Total Profit</h4>
+              </div>
+            </InformationCard>
+          </div>
+        </div>
+        <div className="flex items-end w-1/2 ">
+          <div className="flex flex-col gap-4 bg-surface-700 p-4 rounded-lg w-full justify-center">
+            <h1 className="text-center">Progress</h1>
+            <div className="flex flex-col gap-2">
+              <div className="w-full bg-gray-200 rounded-full h-2.5 mb-4 dark:bg-gray-700">
+                <div className="bg-primary-400 h-2.5 rounded-full " style={{ width: '15%' }} />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="flex flex-row gap-1 md:gap-4 w-full md:w-3/4">
-        <InformationCard>
-          <div className="p-4 bg-[#3a3b64] rounded-full">
-            <RiTruckLine size={35} color="#7367f0" />
-          </div>
-          <div className="flex flex-col text-center gap-1 w-full">
-            <h1 className="text-xl md:text-4xl">9</h1>
-            <h4 className="text-xs md:text-sm">Total Jobs</h4>
-          </div>
-        </InformationCard>
-        <InformationCard>
-          <div className="p-4 bg-[#64633a] rounded-full">
-            <RiPinDistanceLine size={35} color="#ebf55a" />
-          </div>
-          <div className="flex flex-col text-center gap-1 w-full">
-            <h1 className="text-xl md:text-4xl">3,554<span className="text-xl">Km</span></h1>
-            <h4 className="text-xs md:text-sm">Total Distance</h4>
-          </div>
-        </InformationCard>
-        <InformationCard>
-          <div className="p-4 bg-[#3b643a] rounded-full">
-            <RiMoneyEuroCircleLine size={35} color="#2ac926" />
-          </div>
-          <div className="flex flex-col text-center gap-1 w-full">
-            <h1 className="text-xl md:text-4xl">250,310<span className="text-xl">€</span></h1>
-            <h4 className="text-xs md:text-sm">Total Profit</h4>
-          </div>
-        </InformationCard>
-      </div>
+
+
+
       <div className="flex flex-col gap-2 sm:flex-row h-fit w-full md:justify-between">
-        <div className="flex flex-col gap-4 w-full md:w-[540px] h-full bg-surface-700 p-4 rounded-lg items-center">
+        <div className="flex flex-col gap-4 w-full md:w-1/2 h-full bg-surface-700 p-4 rounded-lg items-center">
           <h4>Your weekly Jobs</h4>
           <Chart data={JobsData} />
         </div>
-        <div className="flex flex-col gap-4 w-full md:w-[540px] h-full bg-surface-700 p-4 rounded-lg items-center">
+        <div className="flex flex-col gap-4 w-full md:w-1/2 h-full bg-surface-700 p-4 rounded-lg items-center">
           <h4>Your weekly distance</h4>
           <Chart data={DistanceData} />
         </div>
-        <div className="flex flex-col gap-4 w-full md:w-[540px] h-full bg-surface-700 p-4 rounded-lg items-center">
+        <div className="flex flex-col gap-4 w-full md:w-1/2 h-full bg-surface-700 p-4 rounded-lg items-center">
           <h4>Your weekly profit</h4>
           <Chart data={ProfitData} />
         </div>
       </div>
 
-      <div>
-        <div className="flex flex-col gap-2 bg-surface-700 w-full md:w-2/3 h-full rounded-lg p-4">
+      <div className="flex flex-row gap-2 w-full">
+        <div className="flex flex-col gap-2 bg-surface-700 w-full md:w-full h-full rounded-lg p-4">
           <h1 className="">Latest Jobs</h1>
           <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
             <table className="w-full text-sm text-left rtl:text-right text-gray-400">
@@ -217,6 +237,10 @@ export default function Home() {
               </tbody>
             </table>
           </div>
+        </div>
+
+        <div className="flex flex-col gap-4 w-1/2 h-full">
+          <HomeCarousel />
         </div>
       </div>
 
--- src/components/views/Home/InformationCard.tsx
+++ src/components/views/Home/InformationCard.tsx
@@ -1,7 +1,7 @@
 export default function InformationCard({ children }: Readonly<{ children: React.ReactNode }>) {
     return (
-        <div className=" w-[350px] p-4 bg-surface-700 rounded shadow-md">
-            <div className=" flex flex-col md:flex-row gap-1 md:gap-3 items-center">
+        <div className=" w-[360px] p-4 bg-surface-700 rounded-lg shadow-md">
+            <div className=" flex flex-col md:flex-row items-center w-full">
                 {children}
             </div>
         </div>
