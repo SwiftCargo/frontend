ace4f3bbd84d2f42e0eae78a754c3903afee2bfc
Pikushe <89347562+pikushe@users.noreply.github.com>
2024-06-16 02:34:03 +0300

    Meta data testing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,5 @@
-"use client";
-
 import type { Metadata, Viewport  } from "next";
 import { Poppins, DM_Sans } from "next/font/google";
-import { usePathname } from 'next/navigation';
 import "./globals.css";
 import Header from "@/components/layout/Sidebar";
 import Footer from "@/components/layout/Footer";
@@ -24,13 +21,10 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const router = usePathname();
-
-  const isLoginPage = router === '/login';
   return (
     <html lang="en">
-      <body className={cn("bg-surface-900 flex flex-col md:flex-row  gap-2 md:gap-[15px]", poppins.className, isLoginPage ? 'p-0' : 'p-1 md:p-1.5')}>
-        {!isLoginPage && <Header />}
+      <body className={cn("bg-surface-900 flex flex-col md:flex-row  gap-2 md:gap-[15px]", poppins.className)}>
+        <Header />
         {children}
       </body>
     </html>
