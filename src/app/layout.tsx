0662a63f0905964092013b62fd8caad54ad1c561
Pikushe <89347562+pikushe@users.noreply.github.com>
2024-06-16 02:31:36 +0300

    Swift

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import type { Metadata, Viewport  } from "next";
 import { Poppins, DM_Sans } from "next/font/google";
 import { usePathname } from 'next/navigation';
 import "./globals.css";
@@ -9,6 +10,14 @@ import { cn } from "@/lib/utils";
 
 const poppins = Poppins({ subsets: ["latin"], weight: ['300', '400', '500', '600', '700'] });
 
+export const metadata: Metadata = {
+  title: "Golden Phoenix Express",
+  description: "Golden Phoenix Express is a VTC founded on 24.05.2020 by HandOfClash. It was re-opened on 01.01.2022 with a new vision to create a family like VTC Experience for our drivers A family which is active, friendly and experienced while traversing the roads of Euro Truck Simulator 2 and American Truck Simulator."
+};
+
+export const viewport: Viewport = {
+  themeColor: '#805100',
+}
 
 export default function RootLayout({
   children,
